1348e4d7735213c170e76f2810fe647719603f79
unknown <tarekyosry8@gmail.com>
2023-10-10 20:25:42 +0300

    Add Libraries

--- admin/includes/templates/footer.php
+++ admin/includes/templates/footer.php
@@ -1,5 +1,11 @@
     <div>
         This is Footer
+        <div class="btn btn-danger">test</div>
+        <i class="fa-solid fa-bars"></i>
+        <i class="fa-solid fa-house"></i>
     </div>
+    <script src="https://code.jquery.com/jquery-1.12.1.js" integrity="sha256-VuhDpmsr9xiKwvTIHfYWCIQ84US9WqZsLfR4P7qF6O8=" crossorigin="anonymous"></script>
+    <script src="layout/js/bootstrap.min.js"></script>
+    <script src="layout/js/backend.js"></script>
 </body>
 </html>
