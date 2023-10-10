cf48450acf71e7e0f4c1e4f4c295bbca5e7dc468
unknown <tarekyosry8@gmail.com>
2023-10-11 01:52:18 +0300

    Create form and check for users in DB

--- admin/index.php
+++ admin/index.php
@@ -2,12 +2,34 @@
 include("init.php"); 
 include($tpl . "header.php"); 
 include('includes/languages/english.php');
+
+// Check if user coming from post request
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $username = $_POST['user'];
+    $password = $_POST['pass'];
+    $hashedpass = sha1($password);
+}
+
+// Check if user exist in DB
+
+$stmt = $con->prepare("SELECT Username, Password FROM users WHERE Username = ? AND Password = ? AND GroupID = 1");
+$stmt->execute(array($username, $hashedpass));
+$count = $stmt->rowCount();
+
+// If user exist, Login
+
+if ($count > 0) {
+    echo "Welcome $username";
+}
 ?>
 
-<form class="login">
+
+<form class="login" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
     <h4 class="text-center">Admin Login</h4>
-    <input class="form-control form-control-lg input-lg" type="password" name="pass" placeholder="Password" autocomplete="off">
     <input class="form-control form-control-lg input-lg" type="text" name="user" placeholder="Username" autocomplete="off">
+    <input class="form-control form-control-lg input-lg" type="password" name="pass" placeholder="Password" autocomplete="off">
     
     <div class="d-grid gap-2">
         <input type="submit" value="Login" class="btn btn-lg btn-primary btn-block">
