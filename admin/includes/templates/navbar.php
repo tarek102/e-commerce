42cb31a7631682db25918e7ab1a9e5ed03762f2c
unknown <tarekyosry8@gmail.com>
2023-10-13 02:54:41 +0300

    Edit Navbar

--- admin/includes/templates/navbar.php
+++ admin/includes/templates/navbar.php
@@ -8,9 +8,17 @@
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link" href="#"><?php echo lang("CATEGORIES")?></a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link" href="#"><?php echo lang("ITEMS")?></a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link" href="#"><?php echo lang("MEMBERS")?></a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link" href="#"><?php echo lang("STATISTICS")?></a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link" href="#"><?php echo lang("LOGS")?></a>
         </li>
         
