c1343c19936f755b5e7d2cda941ef0b7134088c4
unknown <tarekyosry8@gmail.com>
2023-10-16 01:47:05 +0300

    Create addTitle func

--- admin/includes/functions/functions.php
+++ admin/includes/functions/functions.php
@@ -2,3 +2,8 @@
 
 // Page title function
 
+function getTitle(){
+    global $pageTitle;
+
+    echo isset($pageTitle) ?  $pageTitle :  'Default';
+}
--- admin/init.php
+++ admin/init.php
@@ -9,10 +9,10 @@ $func = 'includes/functions/';
 $css = 'layout/css/';
 $js = 'layout/js/';
 
-
+include $func . 'functions.php';
 include $lang . 'english.php';
 include $tpl . 'header.php';
-include $func . 'functions.php';
+
 
 
 if (!isset($noNavbar)) {
