372c9ecdfdcc738c1b8a68a04b62920fb275ce6f
unknown <tarekyosry8@gmail.com>
2023-11-01 02:05:48 +0200

    Add category ordering

--- admin/categories.php
+++ admin/categories.php
@@ -15,34 +15,51 @@
         include $tpl . 'footer.php';
         $do = isset($_GET['do']) ? $_GET['do'] : 'Manage';
         if ($do == 'Manage') {
-                $stmt2 = $con->prepare("SELECT * FROM categories");
-                $stmt2->execute();
-                $cats = $stmt2->fetchAll();?>
 
-                <h1 class="text-center">Manage Categories</h1>
-                <div class="container categories">
-                    <div class="card">
-                        <div class="card-body">
-                            <h2 class="card-title">Card title</h2>
-                            <div class="card-text">
-                                <?php
-                                    foreach ($cats as $cat) {
-                                        echo "<div class='cat'>";
-                                        echo "<h3>" . $cat['Name'] . "</h3>";
-                                        echo "<p>"; if ($cat['Description'] == '') { echo 'No Description for this category.' ;} else {echo  $cat['Description'];  } ; echo "</p>";
-                                        if ($cat['Visibility'] === 1) {echo "<span class='visibility'>Hidden</span>";} 
-                                        if ($cat['Allow_Comment'] === 1) {echo "<span class='commenting'>Comment disabled</span>";} 
-                                        if ($cat['Allow_Ads'] === 1) {echo "<span class='advertising'>Ads disabled</span>";} 
-                                        echo "</div>";
-                                        echo "<hr>";
-                                    }
-                                ?>
+            $sort = "ASC";
+            $sort_Array = array("ASC", "DESC");
+            if (isset($_GET['sort']) && in_array($_GET['sort'], $sort_Array)) {
+                $sort = $_GET['sort'];
+            }
+            $stmt2 = $con->prepare("SELECT * FROM categories ORDER BY Ordering $sort");
+            $stmt2->execute();
+            $cats = $stmt2->fetchAll();?>
+
+            <h1 class="text-center">Manage Categories</h1>
+            <div class="container categories">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-title">
+                            <h3>Card title</h3>
+                            <div class="ordering float-end">
+                                Ordering:
+                                <a class='<?php if($_GET['sort'] == "ASC"){echo "active";} ?>' href="?sort=ASC">ASC</a> |
+                                <a class='<?php if($_GET['sort'] == "DESC"){echo "active";} ?>' href="?sort=DESC">DESC</a>
                             </div>
                         </div>
-                        
+                        <div class="card-text">
+                            <?php
+                                foreach ($cats as $cat) {
+                                    echo "<div class='cat'>";
+                                    echo "<div class='hidden-buttons'>";
+                                        echo "<a href='#' class='btn btn-sm btn-primary'><i class='fa fa-edit'></i>Edit</a>";
+                                        echo "<a href='#' class='btn btn-sm btn-danger'><i class='fa fa-close'></i>Delete</a>";
+                                    echo "</div>";
+                                    echo "<h3>" . $cat['Name'] . "</h3>";
+                                    echo "<p>"; if ($cat['Description'] == '') { echo 'No Description for this category.' ;} else {echo  $cat['Description'];  } ; echo "</p>";
+                                    if ($cat['Visibility'] === 1) {echo "<span class='visibility'>Hidden</span>";} 
+                                    if ($cat['Allow_Comment'] === 1) {echo "<span class='commenting'>Comment disabled</span>";} 
+                                    if ($cat['Allow_Ads'] === 1) {echo "<span class='advertising'>Ads disabled</span>";} 
+                                    echo "</div>";
+                                    echo "<hr>";
+                                }
+                            ?>
+                        </div>
                     </div>
+                    
                 </div>
-                <?php
+            </div>
+            <?php
         } elseif ($do == 'Add') {
             ?>
         <h1 class="text-center">Add Category</h1>
