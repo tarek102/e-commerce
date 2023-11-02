e145372796a2ca3a7e47ddea08ebf80ba10fd182
unknown <tarekyosry8@gmail.com>
2023-11-02 02:26:09 +0200

    Edit and Update Categories

--- admin/categories.php
+++ admin/categories.php
@@ -42,7 +42,7 @@
                                 foreach ($cats as $cat) {
                                     echo "<div class='cat'>";
                                     echo "<div class='hidden-buttons'>";
-                                        echo "<a href='#' class='btn btn-sm btn-primary'><i class='fa fa-edit'></i>Edit</a>";
+                                        echo "<a href='categories.php?do=Edit&catid=" . $cat['ID'] ."' class='btn btn-sm btn-primary'><i class='fa fa-edit'></i>Edit</a>";
                                         echo "<a href='#' class='btn btn-sm btn-danger'><i class='fa fa-close'></i>Delete</a>";
                                     echo "</div>";
                                     echo "<h3>" . $cat['Name'] . "</h3>";
@@ -190,9 +190,153 @@
             }
 
         } elseif ($do == 'Edit') {
-            # code...
+            $catid = isset($_GET['catid']) && is_numeric($_GET['catid']) ? 
+            intval($_GET['catid']) : 
+            0;
+            
+            $stmt = $con->prepare("SELECT * FROM categories WHERE ID = ?");
+            $stmt->execute(array($catid));
+            $cat = $stmt->fetch();
+            $count = $stmt->rowCount();
+
+            if ($count > 0) { ?>
+
+            <h1 class="text-center">Edit Categories</h1>
+            <div class="container">
+                <form method="post" action="?do=Update">
+                    <!-- Category Field Start -->
+                    <input type="hidden" name="catid" value="<?php echo $catid; ?>">
+                    <div class="form-group row mb-3">
+                        <label class="col-sm-2 col-form-label">Name</label>
+                        <div class="col-sm-10 col-md-8">
+                            <input type="text" name="name" class="form-control form-control-lg" required="required" placeholder="Add category name" value="<?php echo $cat['Name']?>">
+                        </div>
+                    </div>
+                    <!-- Category Field End -->
+                    <!-- Description Field Start -->
+                    <div class="form-group row mb-3">
+                        <label class="col-sm-2 col-form-label">Description</label>
+                        <div class="col-sm-10 col-md-8">
+                            <input type="text" name="description" class="description form-control form-control-lg"  placeholder="Describe the category" value="<?php echo $cat['Description']?>">
+                        </div>
+                    </div>
+                    <!-- Description Field End -->
+                    <!-- Ordering Field Start -->
+                    <div class="form-group row mb-3">
+                        <label class="col-sm-2 col-form-label ">Ordering</label>
+                        <div class="col-sm-10 col-md-8">
+                            <input type="text" name="ordering" class="form-control  form-control-lg" placeholder="Write the order of the category" value="<?php echo $cat['Ordering']?>">
+                        </div>
+                    </div>
+                    <!-- Ordering Field End -->
+                    <!-- Visibility Field Start -->
+                    <div class="form-group row mb-3">
+                        <label class="col-sm-2 col-form-label">Visible</label>
+                        <div class="col-sm-10 col-md-8">
+                            <div>
+                                <input id="vis-yes" type="radio" name="visibility" value="0" <?php if($cat['Visibility'] == 0 ){ echo "checked";} ?> >
+                                <label for="vis-yes">Yes</label>
+                            </div>
+                            <div>
+                                <input id="vis-no" type="radio" name="visibility" value="1" <?php if($cat['Visibility'] == 1 ){ echo "checked";} ?>>
+                                <label for="vis-no">No</label>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <!-- Visibility Field End -->
+                    <!-- Commenting Field Start -->
+                    <div class="form-group row mb-3">
+                        <label class="col-sm-2 col-form-label">Allow ommenting</label>
+                        <div class="col-sm-10 col-md-8">
+                            <div>
+                                <input id="comm-yes" type="radio" name="commenting" value="0" <?php if($cat['Allow_Comment'] == 0 ){ echo "checked";} ?>>
+                                <label for="comm-yes">Yes</label>
+                            </div>
+                            <div>
+                                <input id="comm-no" type="radio" name="commenting" value="1" <?php if($cat['Allow_Comment'] == 1 ){ echo "checked";} ?>>
+                                <label for="comm-no">No</label>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <!-- Commenting Field End -->
+                    <!-- Ads Field Start -->
+                    <div class="form-group row mb-3">
+                        <label class="col-sm-2 col-form-label">Allow Ads</label>
+                        <div class="col-sm-10 col-md-8">
+                            <div>
+                                <input id="ads-yes" type="radio" name="ads" value="0" <?php if($cat['Allow_Ads'] == 0 ){ echo "checked";} ?>>
+                                <label for="ads-yes">Yes</label>
+                            </div>
+                            <div>
+                                <input id="ads-no" type="radio" name="ads" value="1" <?php if($cat['Allow_Ads'] == 1 ){ echo "checked";} ?>>
+                                <label for="ads-no">No</label>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <!-- Ads Field End -->
+                    <!-- Submit Field Start -->
+                    <div class="form-group row mb-3">
+                        <div class="offset-sm-2 col-sm-10 col-md-8">
+                            <input type="submit" class="btn btn-primary btn-lg" value="Save">
+                        </div>
+                    </div>
+                    <!-- Submit Field End -->
+                </form>
+            </div>
+
+            <?php 
+            } else {
+            $msg = "<div class='alert alert-danger'>False ID</div>";
+            homeRedirect($msg);
+            }
+            
+        echo '</div>';
         } elseif ($do == 'Update') {
-            # code...
+            echo '<h1 class="text-center">Update Member</h1>';
+            echo '<div class="container">';
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+                $id = $_POST['catid'];
+                $name = $_POST['name'];
+                $desc = $_POST['description'];
+                $ordering = $_POST['ordering'];
+                $visiblity = $_POST['visibility'];
+                $commenting = $_POST['commenting'];
+                $ads = $_POST['ads'];
+
+
+                // Update the Database
+
+                
+                $stmt = $con->prepare("UPDATE
+                                        categories
+                                    Set
+                                        Name = ?,
+                                        Description = ?,
+                                        Ordering = ?,
+                                        Visibility = ?,
+                                        Allow_Comment = ?,
+                                        Allow_Ads = ?
+                                    Where
+                                        ID = ?");
+                
+                $stmt->execute(array($name, $desc, $ordering, $visiblity, $commenting, $ads, $id));
+
+                $msg = '<div class="alert alert-primary">' . $stmt->rowCount() . "Updated </div>";
+                homeRedirect($msg, 'back');
+                
+                
+            } else {
+
+                $msg = '<div class="alert alert-danger">You cant access here</div>';
+                homeRedirect($msg);
+                
+            }
+
+            echo '</div>';
         } elseif ($do == 'Delete') {
             # code...
         }
